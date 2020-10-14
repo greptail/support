98b7d57fa7fffc84bf83670e2f5dba6264563726
slooper <shubhanshu.shukla@live.com>
2020-10-14 14:52:34 +0530

    fix

--- src/models/ticket.js
+++ src/models/ticket.js
@@ -843,7 +843,7 @@ ticketSchema.statics.getTicketsWithObject = function (grpId, object, callback) {
     .find({ group: { $in: grpId }, deleted: false })
     .select('-issue -comments -history -subscribers -attachments')
     .populate(
-      'group owner assignee subscribers comments.owner notes.owner history.owner',
+      'group.name owner assignee subscribers comments.owner notes.owner history.owner',
       'username fullname email role image title'
     )
     .populate('assignee', 'username fullname email role image title')
